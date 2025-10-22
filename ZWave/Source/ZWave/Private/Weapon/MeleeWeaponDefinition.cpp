46420670b451188264af33fa8fd9326bb4a5b34f
Hyun <hnjogo@gmail.com>
2025-10-22 19:32:12 +0900

    [Move] Weapon 클래스 Public / Private 폴더 분리

--- ZWave/Source/ZWave/Private/Weapon/MeleeWeaponDefinition.cpp
+++ ZWave/Source/ZWave/Private/Weapon/MeleeWeaponDefinition.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "WeaponDefinition.h"
+#include "Weapon/MeleeWeaponDefinition.h"
 
--- ZWave/Source/ZWave/Private/Weapon/WeaponDefinition.cpp
+++ ZWave/Source/ZWave/Private/Weapon/WeaponDefinition.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "ShootWeaponDefinition.h"
+#include "Weapon/WeaponDefinition.h"
 
