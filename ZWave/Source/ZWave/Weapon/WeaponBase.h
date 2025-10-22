8c9fe902f30b9fceb59e2295205083d3ad42b199
Hyun <hnjogo@gmail.com>
2025-10-22 11:29:09 +0900

    [Add] Weapon Base Class
    - UProject의 Engine 부분 추가

---
+++ ZWave/Source/ZWave/Weapon/WeaponBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WeaponBase.generated.h"
+
+UCLASS()
+class ZWAVE_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AWeaponBase();
+
+};
