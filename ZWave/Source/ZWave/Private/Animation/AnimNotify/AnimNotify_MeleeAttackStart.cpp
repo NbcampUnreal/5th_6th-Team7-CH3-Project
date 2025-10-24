8e4ddeae8f6a4f029e0c73e7eaa68d8d5e1461f7
Hyun <hnjogo@gmail.com>
2025-10-24 17:22:30 +0900

    [Add] Melee Attack 용 Anim Notify Class 추가
    - Start / End

---
+++ ZWave/Source/ZWave/Private/Animation/AnimNotify/AnimNotify_MeleeAttackStart.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/AnimNotify/AnimNotify_MeleeAttackStart.h"
+#include "Weapon/EquipComponent.h"
+#include "Weapon/MeleeWeapon.h"
+
+void UAnimNotify_MeleeAttackStart::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	if (MeshComp == nullptr)
+		return;
+
+	AActor* Owner = MeshComp->GetOwner();
+	if (Owner == nullptr)
+		return;
+
+	UEquipComponent* EquipComponent = Owner->FindComponentByClass<UEquipComponent>();
+	if (EquipComponent == nullptr)
+		return;
+
+	if (AWeaponBase* WeaponActor = EquipComponent->GetCurrentWeapon())
+	{
+		if (AMeleeWeapon* MeleeWeapon = Cast<AMeleeWeapon>(WeaponActor))
+		{
+			MeleeWeapon->StartAttack();
+		}
+	}
+}
