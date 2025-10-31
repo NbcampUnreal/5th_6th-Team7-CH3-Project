a51c5f8b4fab1090ee21387e086628833289c66d
Hyun <hnjogo@gmail.com>
2025-10-31 10:26:38 +0900

    [Update] ModeDefinition 수정
    - Stat 적용 타입 방식 Enum 생성
    - Weapon Stat, Effect Class 포함

--- ZWave/Source/ZWave/Public/Mode/ModeDefinition.h
+++ ZWave/Source/ZWave/Public/Mode/ModeDefinition.h
@@ -1,39 +1,20 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
 #include "Effect/EffectBase.h"
+#include "Weapon/WeaponDefinition.h"
 #include "ModeDefinition.generated.h"
 
 UENUM(BlueprintType)
-enum EEffectType : uint8
+enum class EWeaponModifier : uint8
 {
-	None,
-	Stagger
-};
-
-UENUM(BlueprintType)
-enum EAbilityType : uint8
-{
-	Active,
-	Passive
-};
-
-USTRUCT(BlueprintType)
-struct FEffectStruct
-{
-	GENERATED_BODY()
-public:
-	UPROPERTY(EditDefaultsOnly)
-	float EffectValue;
-
-	UPROPERTY(EditDefaultsOnly)
-	TEnumAsByte<EEffectType> EffectType;
-
-	UPROPERTY(EditDefaultsOnly)
-	TEnumAsByte<EAbilityType> AbilityType;
+	EWM_Add,
+	EWM_Percent,
+	EWM_Multiple,
+	EWM_None,
 };
 
 UCLASS()
@@ -42,13 +23,15 @@ class ZWAVE_API UModeDefinition : public UPrimaryDataAsset
 	GENERATED_BODY()
 
 public:
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Definition")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mode")
 	FName ModeName;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Definition")
-	FEffectStruct EffectStruct;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mode")
+	EWeaponModifier ModeStatApplyType;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Definition")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mode")
+	TSubclassOf<UWeaponDefinition> StatsDef;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mode")
 	TSubclassOf<UEffectBase> EffectClass;
 };
