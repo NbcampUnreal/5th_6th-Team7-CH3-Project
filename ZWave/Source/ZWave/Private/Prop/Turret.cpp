d4b6200443b5f20fcb58df674b577d36a2a0dce9
mw08081 <president304@naver.com>
2025-11-01 10:11:50 +0900

    [Add] Basis attack logic for turret

--- ZWave/Source/ZWave/Private/Prop/Turret.cpp
+++ ZWave/Source/ZWave/Private/Prop/Turret.cpp
@@ -3,12 +3,27 @@
 
 #include "Prop/Turret.h"
 
+#include "Components/SphereComponent.h"
+#include "TimerManager.h"
+
+#include "Weapon/EquipComponent.h"
+#include "Enemy/BaseEnemy.h"
+
 // Sets default values
 ATurret::ATurret()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = MeshComp;
+
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetupAttachment(MeshComp);
+
+	SphereComp->InitSphereRadius(AwarenessRange);
+
+	EquipComp = CreateDefaultSubobject<UEquipComponent>(TEXT("EquipComp"));
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +31,9 @@ void ATurret::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	Health = MaxHealth;
+
+	SphereComp->OnComponentBeginOverlap.AddDynamic(this, &ATurret::OnSphereBeginOverlap);
 }
 
 // Called every frame
@@ -25,3 +43,44 @@ void ATurret::Tick(float DeltaTime)
 
 }
 
+void ATurret::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->IsA(ABaseEnemy::StaticClass()))
+	{
+		UE_LOG(LogTemp, Display, TEXT("Target is overlaped: %s"), *OtherActor->GetActorNameOrLabel());
+		Target = static_cast<ABaseEnemy*>(OtherActor);
+
+		GetWorld()->GetTimerManager().SetTimer(AttackTimerHandle, this, &ATurret::Attack, 5.0f, true);
+	}
+}
+
+void ATurret::Attacked(AActor* DamageCauser, float Damage)
+{
+	ApplyDamage(Damage, false);
+}
+
+void ATurret::ApplyDamage(float Damage, bool CheckArmor)
+{
+	Health -= Damage;
+
+	if (Health <= 0)
+	{
+		Health = 0;
+		Die();
+	}
+}
+
+void ATurret::Die()
+{
+	SetLifeSpan(0.5f);
+}
+
+void ATurret::Attack()
+{
+	if (Target == nullptr) {
+		UE_LOG(LogTemp, Display, TEXT("Null Target"));
+		return;
+	}
+
+	Target->Attacked(this, 20);
+}
--- ZWave/Source/ZWave/Public/Prop/Turret.h
+++ ZWave/Source/ZWave/Public/Prop/Turret.h
@@ -4,10 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+
+#include "Base/Damagable.h"
+
 #include "Turret.generated.h"
 
 UCLASS()
-class ZWAVE_API ATurret : public AActor
+class ZWAVE_API ATurret : public AActor, public IDamagable
 {
 	GENERATED_BODY()
 	
@@ -19,7 +22,52 @@ protected:
 public:	
 	virtual void Tick(float DeltaTime) override;
 
-public:
-	
+	UFUNCTION()
+	void OnSphereBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
 
+public:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UStaticMeshComponent> MeshComp;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class USphereComponent> SphereComp;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UEquipComponent> EquipComp;
+
+
+/// <summary>
+/// 체력, 피격, 사망관련
+/// </summary>
+public:
+	virtual void Attacked(AActor* DamageCauser, float Damage) override;
+	virtual void ApplyDamage(float Damage, bool CheckArmor = true) override;
+	virtual void Die() override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Ability")
+	float MaxHealth;
+
+	UPROPERTY(VisibleAnywhere)
+	float Health;
+
+/// <summary>
+/// 공격 관련
+/// </summary>
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Ability")
+	float AwarenessRange = 1000;
+
+	class ABaseEnemy* Target;
+	FTimerHandle AttackTimerHandle;
+
+protected:
+	virtual void Attack();
 };
