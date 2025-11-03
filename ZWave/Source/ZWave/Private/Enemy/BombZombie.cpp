c01ddf79f025efd59ce320dd754ac1912ac691e5
mw08081 <president304@naver.com>
2025-11-03 20:28:02 +0900

    [Add] bCanOveralpSelf to MeleeAttackDefinition

--- ZWave/Source/ZWave/Private/Enemy/BombZombie.cpp
+++ ZWave/Source/ZWave/Private/Enemy/BombZombie.cpp
@@ -4,13 +4,20 @@
 #include "Enemy/BombZombie.h"
 
 #include "Base/BaseCharacter.h"
+#include "Weapon/WeaponBase.h"
+#include "Weapon/EquipComponent.h"
 
 
 void ABombZombie::BeginPlay()
 {
 	Super::BeginPlay();
+}
 
-	UE_LOG(LogTemp, Display, TEXT("Bomb begin play"));
+void ABombZombie::Die()
+{
+	//SuisideBomb();
+
+	Super::Die();
 }
 
 void ABombZombie::Attacked(AActor* DamageCauser, float Damage)
@@ -18,51 +25,8 @@ void ABombZombie::Attacked(AActor* DamageCauser, float Damage)
 	Super::Attacked(DamageCauser, Damage);
 }
 
-void ABombZombie::ApplyDamage(float Damage, bool CheckArmor)
+void ABombZombie::Suiside()
 {
-	Super::ApplyDamage(Damage, CheckArmor);
+
 }
 
-void ABombZombie::PlayHitAnimMontage(AActor* DamageCauser)
-{
-	Super::PlayHitAnimMontage(DamageCauser);
-}
-
-void ABombZombie::Die()
-{
-	//Bomb();
-
-	Super::Die();
-}
-
-void ABombZombie::Attack()
-{
-	Super::Attack();
-}
-
-void ABombZombie::Bomb()
-{
-	TArray<FHitResult> HitResults;
-	float SphereRadius = this->AttackRange * 2;
-
-	bool bHit = GetWorld()->SweepMultiByChannel(
-		HitResults,
-		GetActorLocation(),
-		GetActorLocation(),
-		FQuat::Identity,
-		ECC_Visibility,                  // 사용할 충돌 채널 설정
-		FCollisionShape::MakeSphere(SphereRadius)
-	);
-
-	if (bHit)
-	{
-		for (const FHitResult& Hit : HitResults)
-		{
-			if (Hit.GetActor()->IsA(ABaseCharacter::StaticClass()))
-			{
-				ABaseCharacter* Target = static_cast<ABaseCharacter*>(Hit.GetActor());
-				//Target->Attacked()
-			}
-		}
-	}
-}
